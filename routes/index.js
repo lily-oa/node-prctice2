199b9a259d8d0eaf1f581efed90f20d8a0fbd498
lily <a0976198586@gmail.com>
2020-10-26 21:25:04 +0800

    修改index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'OA六角學院歡迎您!' });
 });
 
 module.exports = router;
